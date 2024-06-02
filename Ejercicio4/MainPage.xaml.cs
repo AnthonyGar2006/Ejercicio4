a3d18d3d79936c7854d7a12f2fcf4a88f92d8291
50237 <50237@DESKTOP-AC292QU>
2024-06-02 22:46:18 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio4/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace Ejercicio4
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            // Obtener el número ingresado
+            int numero = Convert.ToInt32(Num.Text);
+
+            // Calcular el cuadrado y el cubo del número
+            int cuadrado = numero * numero;
+            int cubo = numero * numero * numero;
+
+            // Mostrar los resultados en los labels
+            Cuadrado.Text = $"El cuadrado de {numero} es: {cuadrado}";
+            Cubo.Text = $"El cubo de {numero} es: {cubo}";
+        }
+    }
+
+}
